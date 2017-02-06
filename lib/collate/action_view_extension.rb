fc58cdd0cd3b995005ca5d9be822f653ff8e02c5
Nicholas Page <npage85@gmail.com>
2017-02-06 12:13:04 -0800

    Added checkbox partial.

--- lib/collate/action_view_extension.rb
+++ lib/collate/action_view_extension.rb
@@ -1,7 +1,18 @@
 module Collate
   module ActionViewExtension
-    def filters_for_group group_key
-      puts controller.inspect
+    def filters_for_group record, group_key
+      groups = record.model.collate_filters ||= {}
+
+      group = groups[group_key]
+
+      filters = group[:filters] ||= []
+    end
+
+    def filter_for filter
+      case filter.component[:type]
+      when 'checkbox'
+        render :partial => "collate/checkbox_field", locals: {filter: filter}
+      end
     end
   end
 end
